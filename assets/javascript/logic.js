4fb206742edc5baf021876013d41b89961626597
Brady Douthit <brady.douthit99@gmail.com>
2019-07-13 17:21:25 -0400

    Changed the projects page and added a contact page

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,5 +1,10 @@
-$("#arrow-icon").click(function() {
+$("#project-arrow").click(function() {
     $('html,body').animate({
         scrollTop: $("#projects").offset().top},
+        'slow');
+});
+$("#contact-arrow").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#contact").offset().top},
         'slow');
 });
