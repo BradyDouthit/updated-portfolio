e828e626522159aab1466b53a3915af871ab9773
Brady Douthit <brady.douthit99@gmail.com>
2019-07-18 12:27:26 -0400

    added a footer for navigation, tweaked arrows

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -8,3 +8,19 @@ $("#contact-arrow").click(function() {
         scrollTop: $("#contact").offset().top},
         'slow');
 });
+$("#contact-crumb").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#contact").offset().top},
+        'slow');
+});
+$("#project-crumb").click(function() {
+    $('html,body').animate({
+        scrollTop: $("#projects").offset().top},
+        'slow');
+});
+$("#about-crumb").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".jumbotron").offset().top},
+        'slow');
+});
+
